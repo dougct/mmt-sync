f389f87b76c41431068e9ede479f6125adc6052d
Douglas Teixeira <dougct@users.noreply.github.com>
2014-10-18 09:56:47 -0300

    Explain the overall structure of the script.

--- Scripts/exec_bench.sh
+++ Scripts/exec_bench.sh
@@ -1,92 +1,106 @@
 #!/bin/bash
 
-#-----------------------------------------------
-#DIRECTORIES
-#-----------------------------------------------
-MAIN_FOLDER=$HOME'/mmt-sync'
-#MAIN_FOLDER=$HOME'/projects/mmt-sync'
-PIN_DIR=$HOME'/pin'
-#PIN_DIR=$HOME'/usr/pin'
+# This script is used to run the benchmarks, call the Pin toll, and run the heuristics.
+# It runs the benchmarks, the run them under Pin, which produces files named 'instru_.txt'.
+# These text files contain the traces from the execution of the benchmarks. Our heuristics
+# take these files as input the simulate the execution of the benchmarks the MMT architecture.
 
-#-----------------------------------------------
-#PIN
-#-----------------------------------------------
+# Call a benchmark.
+function start_bench {
+    $1 $2
+}
+
+#------------
+# DIRECTORIES
+#------------
+MAIN_FOLDER=$HOME'/mmt-sync'
+PIN_DIR=$HOME'/pin'
+
+
+#--------------
+# PIN Variables
+#--------------
 
 PINBIN=$PIN_DIR'/intel64/bin/pinbin -injection child'
-#PINBIN=$PIN_DIR'/intel64/bin/pinbin'
 export LD_LIBRARY_PATH=$PIN_DIR'/intel64/lib-ext/'
 
-#PINBIN=$PIN_DIR'/ia32/bin/pinbin'
-#export LD_LIBRARY_PATH=$PIN_DIR'/ia32/lib-ext/'
-
+# Call the Pin tool passing as a parameter the file with the traces ('instru').
 function exec_pin {
     TOOL1=$MAIN_FOLDER'/Tools/obj-intel64/insList.so'
     $PINBIN -t $TOOL1 -o instru -e $1 -s false -f false -- $2
 }
 
-#-----------------------------------------------
-#HEURISTICS
-#-----------------------------------------------
+#-----------
+# HEURISTICS
+#-----------
 HEURISTIC=$MAIN_FOLDER'/Heuristics/bin'
 HEURPARAM=$MAIN_FOLDER'/Heuristics/parameters'
 HEURINFO=$MAIN_FOLDER'/Heuristics/info'
 HEURISTIC_MAIN=$MAIN_FOLDER'/Execute/heuristic'
 HMODE='1'
 
+# Search the names of the heuristics we want to compare.
 heuristics=`find $HEURISTIC -iname *.so -printf '%f '`
 heuristics=`
 for x in \`echo $heuristics\`; do
     echo \`basename $x .so\`
 done | sort`
 
-heuristics='MinSP_PC'
 
-#-----------------------------------------------
-#HELPFUL
-#-----------------------------------------------
-#bintypes='HardwareOnly FunctionLevel PostDom Structured LongestPath'
+#----------------------------------
+# Helper functions and definitions.
+#----------------------------------
+
+# Types of heuristics.
 bintypes='HardwareOnly FunctionLevel LongestPath'
 
-function lista_files {
-    lista=''
+# Get the list of files that contain the execution traces.
+function files_list {
+    list=''
     i=$1
     while [ $i -le $2 ]
     do
-        lista=$lista'instru'$i'.txt '
+        list=$list'instru'$i'.txt '
         i=$(( $i + 1 ))
     done
-    echo $lista
+    echo $list
 }
 
-#-----------------------------------------------
-#BENCHMARKS
-#-----------------------------------------------
+#-----------
+# Benchmarks
+#-----------
 
 BENCH_BIN='?'
 
+# This variable should be set according to the benchmarks we want to run.
+# That is, if we want to run PARSEC & SPLASH, we should 'source' benchmarks.sh,
+# which contains calls to the benchmarks in PARSEC & SPLASH. And if we want to
+# run our toy benchmarks, then we should 'source' the script in the folder 'mini',
+# which contains calls to the toy benchmarks.
+# TODO: make this a parameter to be set when calling the script.
 source $MAIN_FOLDER/'Scripts/benchmarks.sh'
 #source $MAIN_FOLDER/'mini/minibench.sh'
-
 #source $MAIN_FOLDER/'dougs/dougs-bench.sh'
-#BENCH_BIN='/home/douglas/mmt-sync/dougs/exe/simple'
-#-----------------------------------------------
-#MAIN EXECUTION
-#-----------------------------------------------
+
+#--------------------
+# Main Exection Logic
+#--------------------
 set -e
 
-OUTPUT='saida'$HMODE'.txt'
+OUTPUT='output_tables'$HMODE'.txt'
 
 echo -n '' > $OUTPUT
 
 x=2
-# Change it back to 16
+# The number that controls this loop is the number of threads with which
+# we want to execute the benchmarks and heuristics.
 while [ $x -le 16 ]; do
     echo $x' threads' >> $OUTPUT
     
     for teste in $bench_list; do
         echo $teste >> $OUTPUT
         
-        #For each category of heuristic
+        # For each category of heuristic
         for cur_bintypes in $bintypes; do
             
             if [[ $cur_bintypes == 'HardwareOnly' ]]; then
@@ -101,10 +115,10 @@ while [ $x -le 16 ]; do
                 BENCH_BIN=$BENCHMARKS'/structured'
             fi
             
-            #Execute the benchmark
+            # Execute the benchmark
             start_bench $teste $x
             
-            #Execute the heuristics
+            # Execute the heuristics
             simult=8
             y=0
             for heuristic in $heuristics; do
@@ -116,7 +130,7 @@ while [ $x -le 16 ]; do
                 
                 if [[ -e $HEURPARAM/$heuristic'.txt'  ]]; then
                     while read param; do
-                        $HEURISTIC_MAIN $HMODE 0 $HEURISTIC/$heuristic.so "$param" $(lista_files 1 $x ) > 'tempout'$heuristic"$param"'.txt' &
+                        $HEURISTIC_MAIN $HMODE 0 $HEURISTIC/$heuristic.so "$param" $(files_list 1 $x ) > 'tempout'$heuristic"$param"'.txt' &
                         
                         y=$(( $y + 1 ))
                         if [[ $y -eq $simult ]]; then
@@ -125,7 +139,7 @@ while [ $x -le 16 ]; do
                         fi
                     done < $HEURPARAM/$heuristic'.txt'
                 else
-                    $HEURISTIC_MAIN $HMODE 0 $HEURISTIC/$heuristic.so '1' $(lista_files 1 $x ) > 'tempout'$heuristic'.txt' &
+                    $HEURISTIC_MAIN $HMODE 0 $HEURISTIC/$heuristic.so '1' $(files_list 1 $x ) > 'tempout'$heuristic'.txt' &
                     
                     y=$(( $y + 1 ))
                     if [[ $y -eq $simult ]]; then
@@ -171,6 +185,6 @@ while [ $x -le 16 ]; do
     x=$(( $x * 2 ))
 done
 
-mkdir -p saida
-$MAIN_FOLDER/Scripts/analisar $OUTPUT saida
+mkdir -p output_tables
+$MAIN_FOLDER/Scripts/analisar $OUTPUT output_tables
 
